e34c213cd476ccf7711d46d4764e7234ad0ba7b3
David Carrera <142541347+david-carrerx@users.noreply.github.com>
2024-05-29 18:17:04 -0600

    Configuracion firebase e ios

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_EvqqJSAhxsrhbT9-AE79vV4s2Mdb5CM",
+  authDomain: "altair-6ad57.firebaseapp.com",
+  projectId: "altair-6ad57",
+  storageBucket: "altair-6ad57.appspot.com",
+  messagingSenderId: "788036844674",
+  appId: "1:788036844674:web:0f712a185da3abcd4bc89b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
