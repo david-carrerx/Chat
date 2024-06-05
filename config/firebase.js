72bd1229a56d913af19fb901f3df2b7fb82d16a8
David Carrera <142541347+david-carrerx@users.noreply.github.com>
2024-06-05 16:22:15 -0600

    Chat funcional

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,5 +20,7 @@ const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
+
+export const db = getFirestore();
 
 export { app, auth };
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,61 +1,89 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback, useEffect, useLayoutEffect } from 'react'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { collection, addDoc, orderBy, query, onSnapshot } from 'firebase/firestore';
+import { signOut } from 'firebase/auth';
+import { auth, db } from '../config/firebase';
+import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
 
-export default function Profile({ navigation }) {
-    const [messages, setMessages] = useState([])
+export default function Profile() {
+  const [messages, setMessages] = useState([]);
+  const navigation = useNavigation();
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello gay',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ])
-  }, [])
+  const onSignOut = () => {
+    signOut(auth).catch(error => console.log('Error logging out: ', error));
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10
+          }}
+          onPress={onSignOut}
+        >
+          <AntDesign name="logout" size={24} style={{ marginRight: 10 }} />
+        </TouchableOpacity>
+      )
+    });
+  }, [navigation]);
+
+  useLayoutEffect(() => {
+    const collectionRef = collection(db, 'chats');
+    const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+    const unsubscribe = onSnapshot(q, querySnapshot => {
+      console.log('querySnapshot unsusbscribe');
+      setMessages(
+        querySnapshot.docs.map(doc => ({
+          _id: doc.data()._id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user
+        }))
+      );
+    });
+    return unsubscribe;
+  }, []);
 
   const onSend = useCallback((messages = []) => {
     setMessages(previousMessages =>
-      GiftedChat.append(previousMessages, messages),
-    )
-  }, [])
+      GiftedChat.append(previousMessages, messages)
+    );
+
+    const { _id, createdAt, text, user } = messages[0];
+    
+    if (user._id) { // Asegúrate de que user._id esté definido
+      addDoc(collection(db, 'chats'), {
+        _id,
+        createdAt,
+        text,
+        user
+      });
+    } else {
+      console.error('User ID está undefined, no se puede enviar el mensaje.');
+    }
+  }, []);
 
   return (
     <GiftedChat
       messages={messages}
+      showAvatarForEveryMessage={false}
+      showUserAvatar={false}
       onSend={messages => onSend(messages)}
+      messagesContainerStyle={{
+        backgroundColor: '#fff'
+      }}
+      textInputStyle={{
+        backgroundColor: '#fff',
+        borderRadius: 20,
+      }}
       user={{
-        _id: 1,
+        _id: auth?.currentUser?.email ?? 'anonymous', // Proporciona un valor de respaldo
+        avatar: 'https://i.pravatar.cc/300'
       }}
     />
-  )
+  );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#fff'
-    },
-    button: {
-        backgroundColor: '#DC3545',
-        borderRadius: 5,
-        padding: 10,
-        width: 325, 
-        alignItems: 'center',
-        marginBottom: 10,
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: '500',
-    },
-});
